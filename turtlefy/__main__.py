f1dd1f83aea5088460dbfd74a0ec30bcbfa7c437
Nate Rutan <nathan@composedcloud.com>
2020-06-05 12:47:15 -0600

    Updated __main__.py with relevant info

--- turtlefy/__main__.py
+++ turtlefy/__main__.py
@@ -2,7 +2,7 @@
 
 def main():
     print("Please import turtlefy and set up the client")
-    print("from client import get_turtlefy_client")
+    print("from turtlefy.client import get_turtlefy_client")
     print("\n")
     print("client = get_turtlefy_client('https://base.myshopify.com', token='xxxx')")
 
