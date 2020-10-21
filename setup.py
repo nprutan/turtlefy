45ff9b592047607d04142f34fe30d0987c5052c5
nprutan <nprutan@gmail.com>
2020-10-21 16:30:48 -0600

    Pushed to pypi

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/nprutan/turtlefy'
 EMAIL = 'nathan@composedcloud.com'
 AUTHOR = 'Nate Rutan'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.8.11'
+VERSION = '0.8.12'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
